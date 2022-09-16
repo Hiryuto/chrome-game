19b1fa877c960f31f98a96f2d05d6f901e001edf
Hiryuto <hiramatsu.yuto.highschool@gmail.com>
2022-09-16 15:38:13 +0900

    デフォルト設定

--- src/background.js
+++ src/background.js
@@ -1,39 +1,38 @@
 chrome.runtime.onInstalled.addListener((details) => {
-    if (details.reason === 'install') {
-      chrome.tabs.create({ url: 'installed.html' })
+  if (details.reason === 'install') {
+    chrome.tabs.create({ url: 'installed.html' })
+  }
+  if (details.reason === 'update') {
+    chrome.storage.local.set({ CharacterImg: 'Temp' })
+    chrome.storage.local.set({ TempImg: 'Temp' })
+    chrome.storage.local.set({ authorization: 'Temp' })
+    //chrome.tabs.create({ url: "update.html" });
+    //ゲームの変数をChromeの同期ストレージに作成
+    //プレイヤーステータスの作成
+    var statuss = {
+      level: 1,
+      exp: 0,
+      totalExp: 0,
+      hp: 10,
+      atk: 1,
+      def: 1,
+      spd: 1,
+      point: 100,
+      coin: 0,
     }
-    if (details.reason === 'update') {
-      chrome.storage.local.set({ CharacterImg: 'Temp' })
-      chrome.storage.local.set({ TempImg: 'Temp' })
-      chrome.storage.local.set({ authorization: 'Temp' })
-      //chrome.tabs.create({ url: "update.html" });
-      //ゲームの変数をChromeの同期ストレージに作成
-      //プレイヤーステータスの作成
-      var statuss = {
-        level: 1,
-        exp: 0,
-        totalExp: 0,
-        hp: 10,
-        atk: 1,
-        def: 1,
-        spd: 1,
-        point: 100,
-        coin: 0,
-      }
-      var status = JSON.stringify(statuss)
-      chrome.storage.local.set({
-        gamestatus: status,
-      })
-      //システム
-      var flag = {
-        stage: 1,
-        stageClear: [9, 1],
-      }
-      flags = JSON.stringify(flag)
-      chrome.storage.local.set({ flag: flags })
-      chrome.storage.local.set({ gameInv: 'null' })
-      chrome.storage.local.set({ gameGear: 'null' })
-      chrome.storage.local.set({ gameSkill: 'null' })
+    var status = JSON.stringify(statuss)
+    chrome.storage.local.set({
+      gamestatus: status,
+    })
+    //システム
+    var flag = {
+      stage: 1,
+      stageClear: [1, 1],
     }
-  })
-  
+    flags = JSON.stringify(flag)
+    chrome.storage.local.set({ flag: flags })
+    chrome.storage.local.set({ gameInv: 'null' })
+    chrome.storage.local.set({ gameGear: 'null' })
+    chrome.storage.local.set({ gameSkill: 'null' })
+  }
+})
