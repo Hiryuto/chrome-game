734f2b06e9ad80167daed4acdfc91ab86f322245
Hiryuto <hiramatsu.yuto.highschool@gmail.com>
2022-10-14 15:11:00 +0900

    statusの修正

--- src/js/status.js
+++ src/js/status.js
@@ -1,105 +1,53 @@
-//変数の宣言
-/**
- * ステータスが入る変数です
- * @type {{level: レベル,exp: 経験値,totalExp: 累計経験値,hp: 体力,atk: 攻撃力,def: 防御力,spd: スピード,point:ステータスポイント}}
- */
-let Status
-let setstatus
+import { getStatus } from './global.js'
 
-//chrome.storageから変数を取得
-chrome.storage.local.get([`gamestatus`], function (response) {
-  Status = JSON.parse(response.gamestatus)
-})
-setTimeout(() => {
+async function view() {
+  const Status = await getStatus()
+  console.log(Status)
   textContent('point', Status.point)
   textContent('hp', Status.hp)
   textContent('atk', Status.atk)
   textContent('def', Status.def)
   textContent('spd', Status.spd)
-}, 50)
-
+}
 //ボタン制御
 document.getElementById('mainpage').addEventListener('click', () => {
   window.location.href = 'index.html'
 })
-document.getElementById('addhp').addEventListener('click', () => {
+document.getElementById('addhp').addEventListener('click', async () => {
+  const Status = await getStatus()
   if (Status.point > 0) {
     Status.point--
     Status.hp++
-    setstatus = JSON.stringify(Status)
-    chrome.storage.local.set({
-      gamestatus: setstatus,
-    })
-    chrome.storage.local.get([`gamestatus`], function (response) {
-      Status = JSON.parse(response.gamestatus)
-    })
-    setTimeout(() => {
-      textContent('point', Status.point)
-      textContent('hp', Status.hp)
-      textContent('atk', Status.atk)
-      textContent('def', Status.def)
-      textContent('spd', Status.spd)
-    }, 50)
+    console.log(Status)
+    chrome.storage.local.set({ status: Status })
+    view()
   }
 })
-document.getElementById('addatk').addEventListener('click', () => {
+document.getElementById('addatk').addEventListener('click', async () => {
+  const Status = await getStatus()
   if (Status.point > 0) {
     Status.point--
     Status.atk++
-    setstatus = JSON.stringify(Status)
-    chrome.storage.local.set({
-      gamestatus: setstatus,
-    })
-    chrome.storage.local.get([`gamestatus`], function (response) {
-      Status = JSON.parse(response.gamestatus)
-    })
-    setTimeout(() => {
-      textContent('point', Status.point)
-      textContent('hp', Status.hp)
-      textContent('atk', Status.atk)
-      textContent('def', Status.def)
-      textContent('spd', Status.spd)
-    }, 50)
+    chrome.storage.local.set({ status: Status })
+    view()
   }
 })
-document.getElementById('adddef').addEventListener('click', () => {
+document.getElementById('adddef').addEventListener('click', async () => {
+  const Status = await getStatus()
   if (Status.point > 0) {
     Status.point--
     Status.def += 4
-    setstatus = JSON.stringify(Status)
-    chrome.storage.local.set({
-      gamestatus: setstatus,
-    })
-    chrome.storage.local.get([`gamestatus`], function (response) {
-      Status = JSON.parse(response.gamestatus)
-    })
-    setTimeout(() => {
-      textContent('point', Status.point)
-      textContent('hp', Status.hp)
-      textContent('atk', Status.atk)
-      textContent('def', Status.def)
-      textContent('spd', Status.spd)
-    }, 50)
+    chrome.storage.local.set({ status: Status })
+    view()
   }
 })
-document.getElementById('addspd').addEventListener('click', () => {
+document.getElementById('addspd').addEventListener('click', async () => {
+  const Status = await getStatus()
   if (Status.point > 0) {
     Status.point--
     Status.spd++
-    setstatus = JSON.stringify(Status)
-    chrome.storage.local.set({
-      gamestatus: setstatus,
-    })
-    chrome.storage.local.get([`gamestatus`], function (response) {
-      Status = JSON.parse(response.gamestatus)
-    })
-    setTimeout(() => {
-      textContent('point', Status.point)
-      textContent('hp', Status.hp)
-      textContent('atk', Status.atk)
-      textContent('def', Status.def)
-      textContent('spd', Status.spd)
-    }, 50)
+    chrome.storage.local.set({ status: Status })
+    view()
   }
 })
 
@@ -118,3 +66,5 @@ function textContent(id, message) {
 function innerHTML(id, message) {
   document.getElementById(id).innerHTML = `${message}`
 }
+
+view()
