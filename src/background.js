3285ad43615d11dd122fc40a516887b9ea18319d
Hiryuto <108036759+Hiryuto@users.noreply.github.com>
2022-12-06 16:21:35 +0900

    アクセサリー枠を2個に変更＆コメントアウト追加

--- src/background.js
+++ src/background.js
@@ -18,21 +18,26 @@ chrome.runtime.onInstalled.addListener((details) => {
       point: 100,
       coin: 0,
     }
-    chrome.storage.local.set({
-      status: statusData,
-    })
-    //システム
+    //システムflag
     let flagData = {
       stage: 1,
       stageClear: [1, 1],
     }
     let gameInvData = {
+      //通常アイテム
       0: [{ 1: 1, 2: 1, 3: 0, 0: 999, 5: 1 }],
+      //武器
       1: [{ 2: 1 }],
+      //---装備---
+      //ヘルメット
       2: [{ 0: 1 }],
+      //チェストプレート
       3: [{ 1: 1, 2: 2 }],
+      //ブーツ
       4: [{ 3: 1, 4: 1, 5: 1 }],
+      //アクセサリー
       5: [{ 6: 1 }],
+      //その他
       999: [{}],
     }
     let skillData = {
@@ -46,8 +51,9 @@ chrome.runtime.onInstalled.addListener((details) => {
       Chestplate: null,
       Boots: null,
       Weapon: null,
-      Accessory: null,
+      Accessory: [null, null],
     }
+    chrome.storage.local.set({ status: statusData })
     chrome.storage.local.set({ flag: flagData })
     chrome.storage.local.set({ gameInv: gameInvData })
     chrome.storage.local.set({ gameGear: gameGeardata })
